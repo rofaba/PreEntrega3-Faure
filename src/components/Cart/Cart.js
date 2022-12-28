2ed34c63e334603c860a3781b0690ffdc4ad3dd8
rofaba <97056237+rofaba@users.noreply.github.com>
2022-12-28 17:42:33 -0300

    add cart functions

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,19 +9,32 @@ const Cart = () => {
     <>
       <h1 className="text-center text-4xl p-4"> CARRO DE COMPRAS</h1>
       <div className="grid-col gap-12 py-6 px-16 justify-center">
-        
         {/* acá falta verificar que el carrito conteng productos */}
-       
-        {cartproducts.map((product) => (
-        
-          
-        <p>{product.id} {product.name} {product.price} {product.qty} </p>
-          
-        
-        ))}
-        <br></br>    
-        <button className="p-4 bg-slate-200" onClick={removeItem}> Vaciar Carrito </button>
 
+        <tr >
+          <th className="px-16" > Codigo</th>
+          <th className="px-16" > Nombre producto</th>
+          <th className="px-16" > Precio </th>
+          <th className="px-16" > Cantidad </th>
+          <th className="px-16" > Action </th>
+        </tr>
+        {cartproducts.map((product) => (
+          <>
+            <tr className="grid-col-3" >
+              <td className="px-16" >{product.id} </td>
+              <td className="px-16" >{product.name} </td>
+              <td className="px-16" >{product.price}</td>
+              <td className="px-16" >{product.qty}</td>
+              <td> componente boton eliminar del carro</td>
+            </tr>
+          </>
+        ))}
+
+        <br></br>
+        <button className="p-4 bg-slate-200" onClick={removeItem}>
+          {" "}
+          Vaciar Carrito{" "}
+        </button>
       </div>
     </>
   );
